934a517ef2efe2814d46dffea360edbd355bdb84
nabila rabqaoui <rabqaouinabila@gmail.com>
2024-12-07 20:54:29 +0100

    commit backend

---
+++ src/main/java/ma/projet/Tpgrpc2Application.java
@@ -0,0 +1,13 @@
+package ma.projet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tpgrpc2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tpgrpc2Application.class, args);
+	}
+
+}
